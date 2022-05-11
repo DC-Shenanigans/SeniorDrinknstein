1cfb996e8d4652bda747a16ca53ba9d3324ab4ea
spicydll <mason.schmidgall1@gmail.com>
2022-05-11 05:25:04 +0200

    Add basic config validation (dunno if use)

--- src/configs.py
+++ src/configs.py
@@ -11,3 +11,22 @@ def load_from_json(json_file):
         # TODO: check which display to write to
         print("Error reading/parsing '", json_file, "'")
         return None
+
+# Makes sure ingredients are available
+# In:   Configs as python objects
+# Out:  True if good, False if not (fatal error)
+def validate_config(pumps_config, drinks_config):
+    avail_ing = []
+    try:
+        for p in pumps_config:
+            avail_ing.append(p["ing"])
+        for d in drinks_config:
+            for i in d["ingredients"].keys():
+                if (i not in avail_ing):
+                    print("Error: Ingredients missing")
+                    return False
+        return True
+    except:
+        # TODO: check which display to write to
+        print("Error: Invalid Config")
+        return False
