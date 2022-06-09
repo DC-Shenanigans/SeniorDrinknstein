20e9da848ac76b7b99a858fd6fb71bdc18e77d7a
unknown <ask.wietting@gmail.com>
2022-06-09 16:00:42 -0500

    Pretty print

--- src/screen.py
+++ src/screen.py
@@ -1,3 +1,4 @@
+import time
 import board
 import hd44780
 
@@ -5,6 +6,9 @@ import hd44780
 class ScreenGo():
     def __init__(self):
         self.has_screen = False
+        self.to_write = [[], [], [], []]
+        self.current_screen = [[], [], [], []]
+        self.letter_pause = 0.02
         try:
             self.lcd = hd44780.HD44780()  # https://github.com/bablokb/circuitpython-hd44780
             self.has_screen = True
@@ -15,18 +19,31 @@ class ScreenGo():
         if self.has_screen:
             self.lcd.clear()
 
-            line_index = [1, 3, 2, 4]
+            self.to_write = [[], [], [], []]
+
             count = 0
 
-            print_output = ""
-            for idx, letter in enumerate(message):
+            line = []
+            for idx, letter in enumerate(list(message)):
                 if count > 3:
                     print("message too long for screen")
                     break
-                print_output = print_output + letter
+                line.append(letter)
 
-                if len(print_output) == 20 or idx == len(message) - 1:
-                    self.lcd.write(f"{print_output}", line_index[count])
+                if len(line) == 20 or idx == len(message) - 1:
+                    self.to_write[count] = line
 
-                    print_output = ""
+                    line = []
                     count += 1
+            
+            line_indexes = [1, 2, 3, 4]
+            for line_index, line in enumerate(self.to_write):
+                self.current_screen[line_index] = []
+                for letter_index, letter in enumerate(line):
+                    if len(self.current_screen[line_index]) > letter_index:
+                        self.current_screen[line_index][letter_index] = letter
+                    else:
+                        self.current_screen[line_index].append(letter)
+                    if letter != ' ':
+                        self.lcd.write("".join(self.current_screen[line_index]), line_indexes[line_index])
+                        time.sleep(self.letter_pause)
