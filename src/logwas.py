df812b59756b83bbf379ccecc17606fa6d2c0a41
spicydll <mason.schmidgall1@gmail.com>
2022-06-15 03:16:33 +0200

    Add (untested) stat_drink and stat_pump functions

--- src/logwas.py
+++ src/logwas.py
@@ -1,11 +1,14 @@
 # log was fuck
 # Does logging and stats
+from itertools import count
 import json
 
 # I want to make this fuck so badddd
 # fuck it. I'll revert later if u want
 class fuck:
 
+    drink_count_name = "drink_count"
+    pump_time_name = "pump_time"
     stat_file = None
     stat_obj = None
 
@@ -17,13 +20,29 @@ class fuck:
         except:
             # Probably empty file. Will need to make new one
             self.stat_obj = {
-                "drinks": {},
-                "pumps": {}
+                self.drink_count_name: {},
+                self.pump_time_name: {}
             }
 
-            self.update_statfile()
+            self.__update_statfile()
 
-    def update_statfile(self):
+    def stat_drink(self, drink_name):
+        if not self.stat_obj[self.drink_count_name].has_key(drink_name):
+            self.stat_obj[self.drink_count_name][drink_name] = 1
+        else:
+            self.stat_obj[self.drink_count_name][drink_name] += 1
+
+        self.__update_statfile()
+
+    def stat_pump(self, pump_pin, time_sec):
+        if not self.stat_obj[self.pump_time_name].has_key(pump_pin):
+            self.stat_obj[self.pump_time_name][pump_pin] = time_sec
+        else:
+            self.stat_obj[self.pump_time_name][pump_pin] += time_sec
+
+        self.__update_statfile()
+
+    def __update_statfile(self):
         self.stat_file.seek(0)
         json.dump(self.stat_obj, self.stat_file)
         self.stat_file.truncate()
