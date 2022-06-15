c9a82ba104873487307a495c5c2bcb755c6e9b9c
spicydll <mason.schmidgall1@gmail.com>
2022-06-15 02:51:21 +0200

    start log and stat class

---
+++ src/logwas.py
@@ -0,0 +1,28 @@
+# log was fuck
+# Does logging and stats
+import json
+
+class logs:
+
+    stat_file = None
+    stat_obj = None
+
+    def __init__(self, stat_file):
+        self.stat_file = open(stat_file, "r+")
+        
+        try:
+            self.stat_obj = json.load(self.stat_file)
+        except:
+            # Probably empty file. Will need to make new one
+            self.stat_obj = {
+                "drinks": {},
+                "pumps": {}
+            }
+
+            self.update_statfile()
+
+    def update_statfile(self):
+        self.stat_file.seek(0)
+        json.dump(self.stat_obj, self.stat_file)
+        self.stat_file.truncate()
+        
